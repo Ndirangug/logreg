b8052908db33808861cde7ab76be22931ef97888
Ndirangug <ndirangu.mepawa@gmail.com>
2018-03-08 18:22:50 +0300

    password hash

--- logreg.php
+++ logreg.php
@@ -2,22 +2,32 @@
 session_start();
 	//require connction
 	require("connection.php");
-	session_start();
+	if (isset($_SESSION)) {
+		
+	}
+
+	else{session_start();}
+	
 
 	//check if user registered
 	if (isset($_POST['log_in'])) {
 		extract($_POST);
-
-		$checkQuery = "SELECT * FROM register WHERE email = '$username' AND password = '$password';";
+		$hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+		
+		$checkQuery = "SELECT * FROM register WHERE email = '$username';";
 		$runCheckQuery = mysqli_query($conn, $checkQuery);
+		$result = mysqli_fetch_assoc($runCheckQuery);
+		$passDb = $result['password'];
 		$count = mysqli_num_rows($runCheckQuery);
 
-		if ($count == 1) {
+		if ($count == 1 && password_verify($password, $passDb)) {
 			$_SESSION['userName'] = $username;
 			// header("location: dashboard.php");
 			echo "<h2 style='color:green'> Logging you in.....</h2>";
 			header("location:dashboard.php");
-		}else{
+		}
+			
+		else{
 			echo "<h2 style='color:red'>Username or password you entered is incorrect</h2>";
 		}
 
@@ -27,8 +37,8 @@ session_start();
 	elseif (isset($_POST['register'])) {
 		//register to db
 		extract($_POST);
-
-		$query = "INSERT INTO register (name, email, password) VALUES ('$name', '$email', '$pass')";
+		$hashedPass = password_hash($pass, PASSWORD_BCRYPT);
+		$query = "INSERT INTO register (name, email, password) VALUES ('$name', '$email', '$hashedPass')";
 		$insert = mysqli_query($conn, $query);
 
 		if ($insert) {
